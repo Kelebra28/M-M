363c2f761b6c1cb21e110144de6da8e43cf38150
Kelebra <ricardokelebra@gmail.com>
2023-11-15 02:30:27 -0600

    feat: seccion services complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import AboutUs from './sections/AboutUs'
 import OurCompromise from './sections/OurCompromise'
-import Services from './layouts/Services/Service'
+import Services from './sections/Services/Service'
 
 export default function Home() {
   return (
